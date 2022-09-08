7785d8e55a5036713bb17b0e5e60f65e513ce38b
Krystian <poterek123@gmail.com>
2022-09-08 15:18:22 +0200

    czwartek_2

--- app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
@@ -20,8 +20,6 @@ import com.atakmap.coremap.cot.event.CotPoint;
 import com.atakmap.coremap.log.Log;
 import com.atakmap.coremap.maps.time.CoordinatedTime;
 
-import java.io.IOException;
-
 public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
         OnStateListener {
 
@@ -29,18 +27,20 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
             .getSimpleName();
 
     public static final String SHOW_PLUGIN = "com.atakmap.android.plugintemplate.SHOW_PLUGIN";
-    private final Context pluginContext;
-//    private final View myFragmentView;
-//    private final Button markButton;
-    private final TextView textView;
-//    private final EditText latitudeEditText;
-//    private final EditText longitudeEditText;
 
+    private final TextView textView;
+    private final TextView calculatedPos;
     private final View calculateView;
     private final EditText azimuthEditText;
+    private final EditText hullEditText;
     private final EditText distanceEditText;
-    private final Button calculate_xys;
-    //private final Button refresh_cords;
+    private double azimuth;
+    private double distance;
+    private double hull_angle;
+    private double latitude;
+    private double longitude;
+    private double sum_azimuth;
+    private double thousands_azimuth;
     double Xb;
     double Yb;
 
@@ -49,85 +49,56 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
     public PluginTemplateDropDownReceiver(final MapView mapView,
             final Context context) {
         super(mapView);
-        this.pluginContext = context;
 
         // Remember to use the PluginLayoutInflator if you are actually inflating a custom view
         // In this case, using it is not necessary - but I am putting it here to remind
         // developers to look at this Inflator
 
-//        myFragmentView = PluginLayoutInflater.inflate(pluginContext, R.layout.radiostation_layout, null);
-//
-//        textView = myFragmentView.findViewById(R.id.longLatTextView);
-//        markButton = myFragmentView.findViewById(R.id.markButton);
-//
-//        latitudeEditText = myFragmentView.findViewById(R.id.latitude_editText);
-//        longitudeEditText = myFragmentView.findViewById(R.id.longitude_editText);
-//
-//
-//        /**************************** USTALANIE POŁOŻENIA *****************************/
-//
-//        Double latitude = mapView.getSelfMarker().getPoint().getLatitude();
-//        Double longitude = mapView.getSelfMarker().getPoint().getLongitude();
-//        String longAndLat;
-//
-//        longAndLat = latitude.toString() + ' ' + longitude.toString();
-//        textView.setText(longAndLat);
-//
-//
-//        markButton.setOnClickListener(view -> {
-//            String lat = latitudeEditText.getText().toString();
-//            String lon = longitudeEditText.getText().toString();
-//
-//            CotPoint cotPoint = new CotPoint(Double.parseDouble(lat), Double.parseDouble(lon), 0.0, 2.0, 2.0);
-//            CotEvent cotEvent = new CotEvent();
-//            CoordinatedTime time = new CoordinatedTime();
-//
-//            cotEvent.setUID("TEST ladziak");
-//            cotEvent.setTime(time);
-//            cotEvent.setStart(time);
-//            cotEvent.setHow("h-e");
-//            cotEvent.setType("enemy");
-//            //cotEvent.setDetail();
-//            cotEvent.setStale(time.addMinutes(10));
-//            cotEvent.setPoint(cotPoint);
-//
-//            CotMapComponent.getInternalDispatcher().dispatch(cotEvent);
-//
-//        });
 
 
         /*********************** CALCULATOR ***************************/
 
-        calculateView = PluginLayoutInflater.inflate(pluginContext, R.layout.calculator_layout, null);
+        calculateView = PluginLayoutInflater.inflate(context, R.layout.calculator_layout, null);
         textView = calculateView.findViewById(R.id.longLatTextView);
+        calculatedPos = calculateView.findViewById(R.id.calculated_LongAndLat);
 
         azimuthEditText = calculateView.findViewById(R.id.target_azimuth);
+        hullEditText = calculateView.findViewById(R.id.hull_azimuth);
         distanceEditText = calculateView.findViewById(R.id.target_distance);
 
-        calculate_xys = calculateView.findViewById(R.id.calculate_xys);
-        //refresh_cords = calculateView.findViewById(R.id.refresh_cords);
+        Button calculate_xys = calculateView.findViewById(R.id.calculate_xys);
+        Button refresh_cords = calculateView.findViewById(R.id.refresh_cords);
 
+        latitude = mapView.getSelfMarker().getPoint().getLatitude();
+        longitude = mapView.getSelfMarker().getPoint().getLongitude();
 
-        double latitude = mapView.getSelfMarker().getPoint().getLatitude();
-
-        double longitude = mapView.getSelfMarker().getPoint().getLongitude();
         String longAndLat;
-
         longAndLat = Double.toString(latitude) + ' ' + longitude;
         textView.setText(longAndLat);
 
-
+        /*******CALCULATE********/
         calculate_xys.setOnClickListener(view -> {
 
             String azim = azimuthEditText.getText().toString();
-            double azimuth = Double.parseDouble(azim);
+            azimuth = Double.parseDouble(azim);
+
+            String hull = hullEditText.getText().toString();
+            hull_angle = Double.parseDouble(hull);
 
             String dist = distanceEditText.getText().toString();
-            double distance = Double.parseDouble(dist)/100000;
+            distance = Double.parseDouble(dist)/100000;
 
-            azimuth = azimuth * ((float)3/50);
-            Xb = latitude + (distance * Math.sin(azimuth));
-            Yb = longitude + (distance * Math.cos(azimuth));
+            sum_azimuth = hull_angle + azimuth;
+            if (sum_azimuth > 6000)
+                sum_azimuth = sum_azimuth - 6000;
+
+            thousands_azimuth = (sum_azimuth) * ((float)3/50);
+            Xb = latitude + (distance * Math.sin(thousands_azimuth));
+            Yb = longitude + (distance * Math.cos(thousands_azimuth));
+
+            String calculatedPosition;
+            calculatedPosition = Double.toString(Xb) + ' ' + (Yb);
+            calculatedPos.setText(calculatedPosition);
 
             CotPoint cotPoint = new CotPoint(Xb, Yb, 0.0, 1.0, 1.0);
             CotEvent cotEvent = new CotEvent();
@@ -145,9 +116,16 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
             CotMapComponent.getInternalDispatcher().dispatch(cotEvent);
         });
 
-//        refresh_cords.setOnClickListener(view -> {
-//          textView.setText(longAndLat);
-//        });
+
+
+        /******REFRESH_SELF*******/
+        refresh_cords.setOnClickListener(view -> {
+            latitude = mapView.getSelfMarker().getPoint().getLatitude();
+            longitude = mapView.getSelfMarker().getPoint().getLongitude();
+            String up_longAndLat;
+            up_longAndLat = Double.toString(latitude) + ' ' + longitude;
+            textView.setText(up_longAndLat);
+        });
 
     }
 
